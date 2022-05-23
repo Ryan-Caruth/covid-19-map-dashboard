f5d9a5f37e7993d25ea9eb2b63355438137904c0
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-23 10:16:08 -0600

    Display map on page with proper layout

--- client/src/components/map/DisplayMap.js
+++ client/src/components/map/DisplayMap.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import './displaymap.css'
 
 const DisplayMap = () => {
+  const [coordinates, setcoordinates] = useState([53.126126, 30.318214])
+
   return (
-    <div>Map goes here!</div>
-  )
+    <MapContainer className='mapLayout'
+      center={coordinates}
+      zoom={2}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 }
 
 export default DisplayMap
