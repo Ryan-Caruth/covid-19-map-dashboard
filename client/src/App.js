7c3f49a709dba65451ec9954b45d0bcc0c44afe5
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-23 08:30:19 -0600

    Added folders for prep

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import DisplayMap from './components/map/DisplayMap';
 
 function App() {
   return (
     <div className="App">
       <h1>Covid-19 dashboard</h1>
+      <DisplayMap />
     </div>
   );
 }
---
+++ client/src/components/map/DisplayMap.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayMap = () => {
+  return (
+    <div>Map goes here!</div>
+  )
+}
+
+export default DisplayMap
