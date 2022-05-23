f2ae1c0c9837d3025948532f6d9327e74e7dbe0a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-23 11:11:37 -0600

    Used axois is get Covid data

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
@@ -9,3 +10,18 @@ app.listen(5000, (err) => {
 app.get('/', async (req, res) => {
     res.send('This will be a covid 19 dashboard')
 })
+
+app.get("/api", async (req, res) => {
+    let covidDataCountries = await axios.get(
+      "https://disease.sh/v3/covid-19/jhucsse"
+    );
+
+    let array = []
+    for (let i = 0; i < covidDataCountries.data.length; i++) {
+        array.push(covidDataCountries.data[i])
+    }
+
+    res.send(array)
+    console.log(array);
+})
+
